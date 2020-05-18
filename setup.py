4ddb186f05a293447c75206ee70e9c81463649d6
MagedSaeed <msmj2015@gmail.com>
2020-05-18 21:02:21 +0300

    adding python-requires option (requires >=3.6)

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 import setuptools
 
-zip_safe=False
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 
 setuptools.setup(
 	name="farasapy",
@@ -8,6 +12,8 @@ setuptools.setup(
 	author='MagedSaeed',
 	author_email="mageedsaeed1@gmail.com",
 	description="A Python Wrapper for the well Farasa toolkit",
+	long_description=long_description,
+	long_description__content_type="text/markdonw",
 	url="https://github.com/MagedSaeed/farasapy",
 	packages=['farasa'],
 	package_dir = {'farasa':'farasa'},
@@ -17,6 +23,7 @@ setuptools.setup(
 		"License :: GPL",
 		"Operating System :: cross-platform",
 	],
+	python_requires='>=3.6',
 )
 
 # scripts=['farasa/__init__.py','farasa/farasa_bin/*','farasa/farasa_bin/lib/*','farasa/tmp/*'],
