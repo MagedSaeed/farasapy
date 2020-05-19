44454817ae891b2a3053d30b6c63f0f3cf891a95
MagedSaeed <msmj2015@gmail.com>
2020-05-19 07:02:00 +0300

    fix type in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
 	author_email="mageedsaeed1@gmail.com",
 	description="A Python Wrapper for the well Farasa toolkit",
 	long_description=long_description,
-	long_description_content_type="text/markdonw",
+	long_description_content_type="text/markdown",
 	url="https://github.com/MagedSaeed/farasapy",
 	packages=['farasa'],
 	package_dir = {'farasa':'farasa'},
