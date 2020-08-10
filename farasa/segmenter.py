a84fecd853a43cbdcc37d87d71fb4269ac52eac4
MagedSaeed <mageedsaeed1@gmail.com>
2020-08-10 15:39:25 +0300

    add desegmentation code

--- farasa/segmenter.py
+++ farasa/segmenter.py
@@ -6,3 +6,14 @@ class FarasaSegmenter(FarasaBase):
 
     def segment(self, text):
         return self._do_task(text=text)
+
+    def _desegment_word(self, word: str) -> str:
+        desegmented_word = word.replace("ل+ال+", "لل")
+        if "ال+ال" not in word:
+            desegmented_word = desegmented_word.replace("ل+ال", "لل")
+        desegmented_word = desegmented_word.replace("+", "")
+        desegmented_word = desegmented_word.replace("للل", "لل")
+        return desegmented_word
+
+    def desegment(self, text, separator=" "):
+        return " ".join(self._desegment_word(word) for word in text.split(separator))
