9a41f4e0b6701dd3359469af21f66ebf9cb52bfb
MagedSaeed <mageedsaeed1@gmail.com>
2024-10-04 12:47:09 +0300

    assert bin_path is not None

--- farasa/spellchecker.py
+++ farasa/spellchecker.py
@@ -6,7 +6,12 @@ class FarasaSpellChecker(FarasaBase):
 
     @property
     def command(self):
-        assert not self.interactive, "ERROR: Interactive mode is not supported for FarasaSpellCheck. Kindly use standalone mode."
+        assert (
+            not self.interactive
+        ), "ERROR: Interactive mode is not supported for FarasaSpellCheck. Kindly use standalone mode."
+        assert (
+            self.bin_path is not None
+        ), "The FarasaSpellCheck jar file is not downloadable by farasapy. Please download it from qcri first then add its path as 'binary_path=<path_to_jar>' when creating the Spellchecker object."
         if self.bin_path is not None:
             return self.BASE_CMD + [str(self.bin_path)]
         return self.BASE_CMD + [str(self.bin_dir / "FarasaSpellCheck.jar")]
